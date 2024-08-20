de0c95346e4252702a96ec7f23b7dcf122b8b98c
CuDuyDuc <duccu1403@gmail.com>
2024-08-21 02:08:19 +0700

    connect mongodb successfully & done create api auth

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require("express")
+const cors = require('cors')
+const connectDB = require("./src/configs/connectDB")
+const errorMiddleHandle = require("./src/middlewares/errorMiddleWare")
+const authRouter = require("./src/routers/authRouter")
+
+const app = express()
+app.use(cors())
+require('dotenv').config
+app.use(express.json());
+app.use(errorMiddleHandle)
+const PORT = 3000
+
+app.use('/auth' ,authRouter)
+
+connectDB()
+
+app.listen(PORT, (err) => {
+    if(err) {
+        console.log(err)
+        return
+    }
+
+    console.log(`Server starting at http://localhost:${PORT}`)
+})
+
 
